b3680270a9cc3105bd4e40e208c5c8990e0a0677
Esnobart <offrichterrr@gmail.com>
2025-04-01 16:15:43 +0100

    update slices logic

--- src/redux/planars/slice.js
+++ src/redux/planars/slice.js
@@ -1,42 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 import { getPlanars, getPlanar } from "./operations";
 
 const planarsSlice = createSlice({
     name: "planars",
     initialState: {
-        data: [],
+        data: null,
         loading: false,
-        error: false
+        error: false,
     },
-    extraReducers: (builder) => 
+    extraReducers: (builder) =>
         builder
-          .addCase(getPlanars.pending, state => {
-            state.error = false;
-            state.loading = true;
-          })
-          .addCase(getPlanars.fulfilled, (state, action) => {
-            state.error = false;
-            state.loading = false;
-            state.data = action.payload;
-          })
-          .addCase(getPlanars.rejected, state => {
-            state.error = true;
-            state.loading = false;
-          })
-          .addCase(getPlanar.pending, state => {
-            state.error = false;
-            state.loading = true;
-          })
-          .addCase(getPlanar.fulfilled, (state, action) => {
-            state.error = false;
-            state.loading = false;
-            state.data = action.payload;
-          })
-          .addCase(getPlanar.rejected, state => {
-            state.error = true;
-            state.loading = false;
-          })
-})
+            .addCase(getPlanars.pending, (state) => {
+                state.error = false;
+                state.loading = true;
+            })
+            .addCase(getPlanars.fulfilled, (state, action) => {
+                state.error = false;
+                state.loading = false;
+                state.data = action.payload;
+            })
+            .addCase(getPlanars.rejected, (state) => {
+                state.error = true;
+                state.loading = false;
+            })
+            .addCase(getPlanar.pending, (state) => {
+                state.error = false;
+                state.loading = true;
+            })
+            .addCase(getPlanar.fulfilled, (state, action) => {
+                state.error = false;
+                state.loading = false;
+                state.data = action.payload;
+            })
+            .addCase(getPlanar.rejected, (state) => {
+                state.error = true;
+                state.loading = false;
+            }),
+});
 
 export const { clearRelicsList } = planarsSlice.actions;
 export const planarsReducer = planarsSlice.reducer;
--- src/redux/relics/slice.js
+++ src/redux/relics/slice.js
@@ -1,42 +1,42 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 import { getRelics, getRelic } from "./operations";
 
 const relicsSlice = createSlice({
     name: "relics",
     initialState: {
-        data: [],
+        data: null,
         loading: false,
-        error: false
+        error: false,
     },
-    extraReducers: (builder) => 
+    extraReducers: (builder) =>
         builder
-          .addCase(getRelics.pending, state => {
-            state.error = false;
-            state.loading = true;
-          })
-          .addCase(getRelics.fulfilled, (state, action) => {
-            state.error = false;
-            state.loading = false;
-            state.data = action.payload;
-          })
-          .addCase(getRelics.rejected, state => {
-            state.error = true;
-            state.loading = false;
-          })
-          .addCase(getRelic.pending, state => {
-            state.error = false;
-            state.loading = true;
-          })
-          .addCase(getRelic.fulfilled, (state, action) => {
-            state.error = false;
-            state.loading = false;
-            state.data = action.payload;
-          })
-          .addCase(getRelic.rejected, state => {
-            state.error = true;
-            state.loading = false;
-          })
-})
+            .addCase(getRelics.pending, (state) => {
+                state.error = false;
+                state.loading = true;
+            })
+            .addCase(getRelics.fulfilled, (state, action) => {
+                state.error = false;
+                state.loading = false;
+                state.data = action.payload;
+            })
+            .addCase(getRelics.rejected, (state) => {
+                state.error = true;
+                state.loading = false;
+            })
+            .addCase(getRelic.pending, (state) => {
+                state.error = false;
+                state.loading = true;
+            })
+            .addCase(getRelic.fulfilled, (state, action) => {
+                state.error = false;
+                state.loading = false;
+                state.data = action.payload;
+            })
+            .addCase(getRelic.rejected, (state) => {
+                state.error = true;
+                state.loading = false;
+            }),
+});
 
 export const { clearRelicsList } = relicsSlice.actions;
 export const relicsReducer = relicsSlice.reducer;
